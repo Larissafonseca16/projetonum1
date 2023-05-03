f393c8ae5d476725ee2f0bdac0f9fc7be868cebb
Larissafonseca16 <130614152+Larissafonseca16@users.noreply.github.com>
2023-05-03 16:23:17 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+idade=int(input("Informe sua idade: "))
+
+if idade==18:
+    print("voce pode dirigir")
+elif idade >=18:
+    print("voce pode dirigir")
+else :
+    print("voce nao pode dirigir")
